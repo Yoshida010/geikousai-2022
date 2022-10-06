3c501cdabcdaf1e843b229344f42ff5c91e16094
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-10-06 19:58:53 +0900

    修正 ファイルの修正

--- js/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("humbMenuIcon").onclick = () => {
-	document.getElementById("spMenu").classList.toggle("menu-open");
-	document.getElementById("header").classList.toggle("menu-open");
-};
